df7c9b93cdade022c5fcf935ac5c187e45231395
DaYroXy <tmureckless@gmail.com>
2023-10-09 06:43:27 +0300

    add markdown editor

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,5 +1,8 @@
 'use client'
-import { Button, TextArea, TextField } from "@radix-ui/themes"
+
+import { Button,  TextField } from "@radix-ui/themes"
+import SimpleMDE from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
 
 const NewIssuePage = () => {
   return (
@@ -8,9 +11,9 @@ const NewIssuePage = () => {
         <TextField.Root>
             <TextField.Input placeholder="Title" />
         </TextField.Root>
-        <TextArea placeholder="Description" />
+        <SimpleMDE placeholder="Description" />
         <Button>Submit New Issue</Button>
-        
+
     </div>
   )
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,10 +3,12 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import NavBar from './NavBar'
-import { Theme } from '@radix-ui/themes';
+import { Theme, ThemePanel } from '@radix-ui/themes';
 
-const inter = Inter(
-  { subsets: ['latin'] })
+const inter = Inter({ 
+  subsets: ['latin'],
+  variable: '--font-inter'
+})
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -20,8 +22,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <Theme>
+      <body className={inter.variable}>
+      <Theme appearance="light" accentColor="violet">
           <NavBar />
           <main className='px-5'>
             {children}
