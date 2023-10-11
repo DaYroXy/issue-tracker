9173fd51b594390f00cbef3b15e5cbe1ced4fd1d
DaYroXy <tmureckless@gmail.com>
2023-10-11 09:50:11 +0300

    paginate issue

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -6,10 +6,16 @@ import delay from "delay";
 import IssueActions from "./IssueActions";
 import { Issue, Status } from "@prisma/client";
 import { ArrowDownIcon, ArrowUpIcon } from "@radix-ui/react-icons";
+import Pagination from "../componenets/Pagination";
 
 
 interface Props {
-  searchParams: { status: Status, orderBy: keyof Issue, orderDirection: 'asc' | 'desc' }
+  searchParams: {
+    status: Status,
+    orderBy: keyof Issue,
+    orderDirection: 'asc' | 'desc',
+    page: string
+  }
 }
 
 
@@ -32,12 +38,19 @@ const IssuesPage = async ({ searchParams }: Props) => {
     columns.map(column => column.value)
       .includes(searchParams.orderBy) ? { [searchParams.orderBy]: isValidOrderDirection }
       : undefined
+  const where = { status }
 
-  console.log(orderBy)
+  const page = parseInt(searchParams.page) || 1;
+  const pageSize = 10;
+
   const issues = await prisma.issue.findMany({
-    where: { status: status },
-    orderBy
+    where,
+    orderBy,
+    skip: (page - 1) * pageSize,
+    take: pageSize
   });
+
+  const issueCount = await prisma.issue.count({ where })
 
   return (
     <div>
@@ -69,6 +82,7 @@ const IssuesPage = async ({ searchParams }: Props) => {
           ))}
         </Table.Body>
       </Table.Root>
+      <Pagination pageSize={pageSize} currentPage={page} itemCount={issueCount} />
     </div>
   )
 }
