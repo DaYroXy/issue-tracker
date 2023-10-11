d58ae933c9060bce6b74fcdc4edf9c189a369f73
DaYroXy <tmureckless@gmail.com>
2023-10-11 09:44:32 +0300

    pagination

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import Pagination from "./componenets/Pagination";
 
-export default function Home() {
+export default function Home({ searchParams }: { searchParams: { page: string } }) {
+
   return (
     <div>
-      <Pagination itemCount={100} pageSize={10} currentPage={10} />
+      <Pagination itemCount={100} pageSize={10} currentPage={parseInt(searchParams.page)} />
     </div>
   )
 }
